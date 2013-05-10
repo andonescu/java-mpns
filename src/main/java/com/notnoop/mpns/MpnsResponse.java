09efba835983cbec103c3dd61f312651d034d7b5
Ionut Andonescu <ionut.andonescu@endava.com>
2013-05-10 15:16:51 +0300

    comments update in MpnsResponse.java

--- src/main/java/com/notnoop/mpns/MpnsResponse.java
+++ src/main/java/com/notnoop/mpns/MpnsResponse.java
@@ -32,128 +32,132 @@ package com.notnoop.mpns;
 
 /**
  * Represents the logical response of MpnsService
+ * 
+ * for more info look for this
+ * http://msdn.microsoft.com/en-us/library/windowsphone/develop/ff941100
+ * %28v=vs.105%29.aspx
  */
 public enum MpnsResponse {
-    /*
-     * The notification request was accepted and queued for delivery.
-     */
-    RECEIVED(200, "Received", "Connected", "Active", true, false),
+	/*
+	 * The notification request was accepted and queued for delivery.
+	 */
+	RECEIVED(200, "Received", "Connected", "Active", true, false),
 
-    /**
-     * The notification request was accepted and queued for delivery. However,
-     * the device is temporarily disconnected.
-     */
-    QUEUED(200, "Received", "Temporarily Disconnected", "Active", true, false),
+	/**
+	 * The notification request was accepted and queued for delivery. However,
+	 * the device is temporarily disconnected.
+	 */
+	QUEUED(200, "Received", "Temporarily Disconnected", "Active", true, false),
 
-    /**
-     * Queue overflow. The web service should re-send the notification later.
-     * A best practice is to use an exponential backoff algorithm in minute
-     * increments.
-     */
-    QUEUE_FULL(200, "QueueFull", null, "Active", false, true),
+	/**
+	 * Queue overflow. The web service should re-send the notification later. A
+	 * best practice is to use an exponential backoff algorithm in minute
+	 * increments.
+	 */
+	QUEUE_FULL(200, "QueueFull", null, "Active", false, true),
 
-    /**
-     * The push notification was received and dropped by the Push Notification
-     * Service. The Suppressed status can occur if the notification channel
-     * was configured to suppress push notifications for a particular push
-     * notification class.
-     */
-    SUPPRESSED(200, "Suppressed", null, "Active", false, false),
+	/*
+	 * The push notification was received and dropped by the Push Notification
+	 * Service. The Suppressed status can occur if the notification type was not
+	 * enabled by calling BindToShellTile or BindToShellToast in the client
+	 * application, if the raw notification was sent when the application was
+	 * not in the foreground or if a Tile notification was sent with no Tile for
+	 * the application pinned to start.
+	 */
+	SUPPRESSED(200, "Suppressed", null, "Active", false, false),
 
-    /**
-     * This error occurs when the web service sends a notification request
-     * with a bad XML document or malformed notification URI.
-     */
-    BAD_REQUEST(400, null, null, null, false, false),
+	/**
+	 * This error occurs when the web service sends a notification request with
+	 * a bad XML document or malformed notification URI.
+	 */
+	BAD_REQUEST(400, null, null, null, false, false),
 
-    /**
-     * Sending this notification is unauthorized. This error can occur for one
-     * of the following reasons:
-     */
-    UNAUTHORIZED(401, null, null, null, false, false),
+	/**
+	 * Sending this notification is unauthorized. This error can occur for one
+	 * of the following reasons:
+	 */
+	UNAUTHORIZED(401, null, null, null, false, false),
 
-    /**
-     * The subscription is invalid and is not present on the Push Notification
-     * Service. The web service should stop sending new notifications to this
-     * subscription, and drop the subscription state for its corresponding
-     * application session.
-     */
-    EXPIRED(404, "Dropped", null, "Expired", false, false),
+	/**
+	 * The subscription is invalid and is not present on the Push Notification
+	 * Service. The web service should stop sending new notifications to this
+	 * subscription, and drop the subscription state for its corresponding
+	 * application session.
+	 */
+	EXPIRED(404, "Dropped", null, "Expired", false, false),
 
-    /**
-     * Invalid method (PUT, DELETE, CREATE). Only POST is allowed when sending
-     * a notification request.
-     */
-    METHOD_NOT_ALLOWED(405, null, null, null, false, false),
+	/**
+	 * Invalid method (PUT, DELETE, CREATE). Only POST is allowed when sending a
+	 * notification request.
+	 */
+	METHOD_NOT_ALLOWED(405, null, null, null, false, false),
 
-    /**
-     * This error occurs when an unauthenticated web service has reached the
-     * per day throttling limit for a subscription. The web service can try to
-     * re-send the push notification every one hour after receiving this
-     * error. The web service may need to wait up to 24 hours before normal
-     * notification flow will resume.
-     */
-    OVER_LIMIT(406, "Dropped", null, "Active", false, true),
+	/**
+	 * This error occurs when an unauthenticated web service has reached the per
+	 * day throttling limit for a subscription. The web service can try to
+	 * re-send the push notification every one hour after receiving this error.
+	 * The web service may need to wait up to 24 hours before normal
+	 * notification flow will resume.
+	 */
+	OVER_LIMIT(406, "Dropped", null, "Active", false, true),
 
-    /**
-     * The device is in an inactive state. The web service may re-attempt
-     * sending the request one time per hour at maximum after receiving this
-     * error. If the web service violates the maximum of one re-attempt per
-     * hour, the Push Notification Service will de-register or permanently
-     * block the web service.
-     */
-    INACTIVATE_STATE(412, "Dropped", "Inactive", null, false, true),
+	/**
+	 * The device is in an inactive state. The web service may re-attempt
+	 * sending the request one time per hour at maximum after receiving this
+	 * error. If the web service violates the maximum of one re-attempt per
+	 * hour, the Push Notification Service will de-register or permanently block
+	 * the web service.
+	 */
+	INACTIVATE_STATE(412, "Dropped", "Inactive", null, false, true),
 
-    /**
-     * The Push Notification Service is unable to process the request. The web
-     * service should re-send the notification later. A best practice is to
-     * use an exponential backoff algorithm in minute increments.
-     */
-    SERVICE_UNAVAILABLE(503, null, null, null, false, true);
+	/**
+	 * The Push Notification Service is unable to process the request. The web
+	 * service should re-send the notification later. A best practice is to use
+	 * an exponential backoff algorithm in minute increments.
+	 */
+	SERVICE_UNAVAILABLE(503, null, null, null, false, true);
 
-    //// Response Code,NotificationStatus,DeviceConnectionStatus,SubscriptionStatus,Comments
-    private final int responseCode;
-    private final String notificationStatus;
-    private final String deviceConnectionStatus;
-    private final String subscriptionStatus;
+	// // Response
+	// Code,NotificationStatus,DeviceConnectionStatus,SubscriptionStatus,Comments
+	private final int responseCode;
+	private final String notificationStatus;
+	private final String deviceConnectionStatus;
+	private final String subscriptionStatus;
 
-    private final boolean success;
-    private final boolean shouldRetry;
+	private final boolean success;
+	private final boolean shouldRetry;
 
-    MpnsResponse(int responseCode, String notificationStatus,
-            String deviceConnectionStatus,
-            String subscriptionStatus,
-            boolean success,
-            boolean requiresRetry) {
-        this.responseCode = responseCode;
-        this.notificationStatus = notificationStatus;
-        this.deviceConnectionStatus = deviceConnectionStatus;
-        this.subscriptionStatus = subscriptionStatus;
-        this.success = success;
-        this.shouldRetry = requiresRetry;
-    }
+	MpnsResponse(int responseCode, String notificationStatus, String deviceConnectionStatus, String subscriptionStatus,
+			boolean success, boolean requiresRetry) {
+		this.responseCode = responseCode;
+		this.notificationStatus = notificationStatus;
+		this.deviceConnectionStatus = deviceConnectionStatus;
+		this.subscriptionStatus = subscriptionStatus;
+		this.success = success;
+		this.shouldRetry = requiresRetry;
+	}
 
-    public int getResponseCode() {
-        return responseCode;
-    }
+	public int getResponseCode() {
+		return responseCode;
+	}
 
-    public String getNotificationStatus() {
-        return notificationStatus;
-    }
+	public String getNotificationStatus() {
+		return notificationStatus;
+	}
 
-    public String getDeviceConnectionStatus() {
-        return deviceConnectionStatus;
-    }
+	public String getDeviceConnectionStatus() {
+		return deviceConnectionStatus;
+	}
 
-    public String getSubscriptionStatus() {
-        return subscriptionStatus;
-    }
+	public String getSubscriptionStatus() {
+		return subscriptionStatus;
+	}
 
-    public boolean isSuccessful() {
-        return success;
-    }
+	public boolean isSuccessful() {
+		return success;
+	}
 
-    public boolean shouldRetry() {
-        return shouldRetry;
-    }
+	public boolean shouldRetry() {
+		return shouldRetry;
+	}
 }
